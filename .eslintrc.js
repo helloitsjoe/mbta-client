dd0c023b6f094ba22db8295d53408030143b19ff
helloitsjoe <jboyle617@gmail.com>
2018-12-09 09:23:27 -0500

    eslint prettier

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'extends': 'airbnb',
+    'env': {
+        'jest': true
+    },
+    'rules': {
+        'no-console': 'off',
+        'object-curly-newline': 'off',
+        'arrow-parens': ['error', 'as-needed'],
+        'comma-dangle': ['error', 'always-multiline']
+    }
+};
